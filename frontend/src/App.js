f981e1968c8cb2da7616125083ff14facd141e4b
arye321 <arye321@users.noreply.github.com>
2022-08-07 15:36:04 +0000

    google login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
+import React, { useState, useRef } from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
+import jwt_deocde from "jwt-decode";
 
 function App() {
+  const [user, setuser] = useState('xd');
+
   return (
     <div className="App">
       <header className="App-header">
-      <h3>whats in here 😎...</h3>
+      <h3>whats in here 😎....</h3>
+      <h1>{user}</h1>
+      <GoogleOAuthProvider clientId="850364421534-9f2re6djtki6sm8dh02ml8j0dg11fvih.apps.googleusercontent.com">
+     
+        <GoogleLogin
+          text="signin_with"
+          theme="filled_blue"
+          size="large"
+          ux_mode="popup"
+          onSuccess={(credentialResponse) => {
+            console.log(credentialResponse);
+            let userCred = credentialResponse.credential;
+            let payload = jwt_deocde(userCred);
+            console.log(payload);
+            
+          }}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
+      </GoogleOAuthProvider>
       </header>
     </div>
   );
